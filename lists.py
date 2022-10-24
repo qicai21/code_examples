f231abf017d370c6e34e108d72aaec2f746b7db9
qicai21 <qicai21@gmail.com>
2022-10-25 01:56:32 +0800

    Using (non)csrf mode app in UT, add row elements in home page,  and test it in FT.

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 
 lists_bp = Blueprint('lists', __name__)
 
 @lists_bp.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', new_item_text=request.form.get('item_text'))
--- tests.py
+++ tests.py
@@ -5,7 +5,9 @@ import pytest
 @pytest.fixture()
 def app():
     app = create_app()
+    app.config['WTF_CSRF_ENABLED'] = False
     yield app
+
 
 @pytest.fixture()
 def capture_template_while_render(app):
@@ -17,12 +19,22 @@ def capture_template_while_render(app):
         yield template_used
     finally:
         template_rendered.disconnect(capture_template, app)
-    
+
+
 def test_use_home_template(app, capture_template_while_render):
     app.test_client().get('/')
     template_used = capture_template_while_render[0]
     assert template_used == 'home.html'
 
-def test_return_403_while_post_without_csrf(app):
+
+def test_return_403_while_post_without_csrf():
+    app = create_app()
     response = app.test_client().post('/')
     assert response.status_code == 403
+
+def test_can_save_a_post_request(app, capture_template_while_render):
+    text =  'A new list item'
+    client = app.test_client()
+    response = client.post('/', data={'item_text': text}) 
+    assert 'A new list item' in response.data.decode('utf-8')
+    assert capture_template_while_render[0] == 'home.html'
