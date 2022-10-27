3e36a5e6a1e5b3a38bf7b9e2ad3efe68729eefa4
qicai21 <qicai21@gmail.com>
2022-10-28 00:48:47 +0800

    Add Item model, and successfully mapped to mongodb.

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,11 @@ class NewVistorTest(unittest.TestCase):
         self.browser.save_full_page_screenshot('./functest_result.png')
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later (self):
 
         # Edith has heard about a cool new online to-do app. She goes
@@ -43,10 +48,7 @@ class NewVistorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -57,13 +59,9 @@ class NewVistorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
--- tests.py
+++ tests.py
@@ -1,12 +1,20 @@
 from flask import template_rendered
 from app import create_app
+from models import Item
+from mongoengine import connect, disconnect
 import pytest
 
 @pytest.fixture()
 def app():
     app = create_app()
     app.config['WTF_CSRF_ENABLED'] = False
+    disconnect()
+    connect(
+        db='mongoenginetest', 
+        host='mongomock://localhost',
+        uuidRepresentation="pythonLegacy")
     yield app
+    disconnect()
 
 
 @pytest.fixture()
@@ -38,3 +46,19 @@ def test_can_save_a_post_request(app, capture_template_while_render):
     response = client.post('/', data={'item_text': text}) 
     assert 'A new list item' in response.data.decode('utf-8')
     assert capture_template_while_render[0] == 'home.html'
+
+def test_saving_and_retrieving_items(app):
+    first_item = Item()
+    first_item.text = 'The first (ever) list item'
+    first_item.save()
+
+    second_item = Item()
+    second_item.text = 'Item the second'
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+    assert first_saved_item.text == 'The first (ever) list item'
+    assert second_saved_item.text == 'Item the second'
